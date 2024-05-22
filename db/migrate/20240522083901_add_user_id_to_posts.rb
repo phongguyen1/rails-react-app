4823dbf60a54de1d4b0052bb2c0bdcae704017cd
IFISOLUTION\ndphong1 <phong.nguyendinh@emeal.nttdata.com>
2024-05-22 16:27:41 +0700

    create user table

--- db/migrate/20240522083901_add_user_id_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToPosts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :posts, :user_id, :int
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_083901) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_092454) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,11 +44,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_083901) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
